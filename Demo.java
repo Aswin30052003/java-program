9eb53eaa08fd29d44dad2681ee7f263ab6f7e6a9
Aswin <aswinganesan30@gmail.com>
2025-11-23 11:16:46 +0530

    java files

---
+++ Demo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        if (true) {
+            
+        
+        Scanner scan = new Scanner(System.in);
+        String aswin=scan.nextLine();
+        String balu=scan.nextLine();
+
+        if (false) {
+        } 
+        else {
+            
+        }
+
+        System.out.println("pass"+ aswin );
+        System.out.println("fail"+ balu );
+       
+    }
+}
+}
---
+++ hello.java
@@ -0,0 +1,11 @@
+ 
+    class hello{
+          public static void main(String[] args){
+        int a = 109;
+        int b= 20;
+        int c= 50;
+        int d=a+b*c;
+        
+        System.out.println(d);
+    }
+}
